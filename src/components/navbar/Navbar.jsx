396038da29b46d3a467fd99f7fccf146ecccf2ad
David <dwusu@ymail.com>
2022-03-20 02:23:14 +0200

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useContext } from "react";
+import {
+  SearchOutlined,
+  LanguageOutlined,
+  DarkModeOutlined,
+  FullscreenExitOutlined,
+  NotificationsNoneOutlined,
+  ChatBubbleOutlineOutlined,
+  ListOutlined,
+} from "@mui/icons-material";
+import "./Navbar.scss";
+import { DarkModeContext } from "../../context/darkModeReducer";
+
+const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlined className="icon" />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined className="icon" />
+            English
+          </div>
+          <div className="item" onClick={() => dispatch({ type: "TOGGLE" })}>
+            <DarkModeOutlined className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlined className="icon" />
+          </div>
+          <div className="item">
+            <img src="" alt="" className="avatar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,105 @@
+import React, { useContext } from "react";
+import {
+  AccountCircleOutlined,
+  CreditCard,
+  Dashboard,
+  ExitToApp,
+  InsertChart,
+  LocalShipping,
+  NotificationsNone,
+  PersonOutline,
+  PsychologyOutlined,
+  SettingsApplications,
+  SettingsSystemDaydreamOutlined,
+  Store,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import "./Sidebar.scss";
+import { DarkModeContext } from "../../context/darkModeReducer";
+
+const Sidebar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">wusuadmin</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <Dashboard className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutline className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <Store className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
+          <li>
+            <CreditCard className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShipping className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChart className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNone className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlined className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlined className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplications className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlined className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToApp className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
